411d612bfa2e6d0876d7644050cfda726eb11aea
cvam0000 <cvam0000@gmail.com>
2020-03-03 01:44:20 +0530

    index.php

--- index.php
+++ index.php
@@ -66,14 +66,14 @@
 session_start();
 require 'core/include.php';
 
-echo "hello cvam here ";
+
 
 generateRSS();
 $episodes = getEpisodes(null);
 // Backwards comp
-$link = str_replace('?', '', $config['link']);
-$link = str_replace('=', '', $link);
-$link = str_replace('$url', '', $link);
+//$link = str_replace('?', '', $config['link']);
+//$link = str_replace('=', '', $link);
+//$link = str_replace('$url', '', $link);
 
 if(strtolower($config['max_recent']) != 'all') {
     $episodes = array_slice($episodes, 0, $config['max_recent']);
